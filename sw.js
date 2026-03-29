7182f40c1c74b968c622c7f14f647fd74e135885
kaduz1k <kdzsxhp@gmail.com>
2026-03-28 21:25:24 -0300

    Add files via upload
    substituição

--- sw.js
+++ sw.js
@@ -1,41 +1,23 @@
-const CACHE = 'fluxo-v3';
-const ASSETS = [
-  './',
-  './index.html',
-  './icon-192.png',
-  './icon-512.png',
-  './manifest.json'
-];
-
+const CACHE = 'value-v1';
+const ASSETS = ['./', './index.html', './icon-192.png', './icon-512.png', './manifest.json'];
 self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE).then(c => {
-      return Promise.allSettled(ASSETS.map(a => c.add(a).catch(() => {})));
-    })
-  );
+  e.waitUntil(caches.open(CACHE).then(c => Promise.allSettled(ASSETS.map(a => c.add(a).catch(()=>{})))));
   self.skipWaiting();
 });
-
 self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    )
-  );
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
   self.clients.claim();
 });
-
 self.addEventListener('fetch', e => {
   e.respondWith(
     caches.match(e.request).then(cached => {
-      if (cached) return cached;
-      return fetch(e.request).then(response => {
-        if (response && response.status === 200 && response.type !== 'opaque') {
-          const clone = response.clone();
-          caches.open(CACHE).then(c => c.put(e.request, clone));
+      if(cached) return cached;
+      return fetch(e.request).then(res => {
+        if(res && res.status===200 && res.type!=='opaque'){
+          caches.open(CACHE).then(c=>c.put(e.request,res.clone()));
         }
-        return response;
-      }).catch(() => cached);
+        return res;
+      }).catch(()=>cached);
     })
   );
 });
